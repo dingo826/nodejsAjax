fd09549290a5b5d581306e2aaa4f5b56c8587503
dingo826 <23360147@qq.com>
2017-07-27 11:15:47 +0800

    '满足excel的数据处理完成'

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ var https = require('https'),
 
 
 fnLoad();
+var symbolArr = ['yunbibtccny', 'yunbiethcny', 
+				'yunbidgdcny', 'yunbieoscny', 'yunbisccny',
+				'yunbi1stcny', 'yunbianscny', 'yunbietccny', 
+				'yunbigntcny', 'yunbiqtumcny', 'yunbibtscny', 'yunbigxscny']
 
-function fnLoad(){
-	https.get("https://plugin.sosobtc.com/widgetembed/data/period?symbol=yunbisccny&step=900", function(res) {
+//step=60     1分钟
+//step=180    3分钟
+//step=300    5分钟
+//step=600    10分钟
+//step=900    15分钟
+//step=1800   30分钟
+//step=3600   1小时
+//step=7200   2小时
+//step=14400  4小时
+//step=21600  6小时
+//step=43200  12小时
+//step=86400  1天
+//step=259200 3天
+//step=604800 周线
+symbolArr.forEach(function(e){
+	fnLoad(e)
+});
+function fnLoad(name){
+
+	https.get("https://plugin.sosobtc.com/widgetembed/data/period?symbol="+name+"&step=900", function(res) {
 		var size = 0,
 			chunks = [];
 		res.on('data', function(chunk){
@@ -16,15 +38,15 @@ function fnLoad(){
 		res.on('end', function(e){
 			data = Buffer.concat(chunks, size),
 			dataJSON = data.toString();
-			console.log(dataJSON)
-			fs.writeFile('22.json', dataJSON, function(err){
+			console.log(name)
+			fs.writeFile('json/'+name+'.json', dataJSON, function(err){
 				console.log('err' +err)
 			});	
 		});
-		http.createServer(function(req,res){
+		/*http.createServer(function(req,res){
 			res.writeHead(200, {'Content-type' : 'text/html'});
 			res.write(dataJSON);
 			res.end();
-		}).listen(6000);
+		}).listen(6000);*/
 	});
 }
